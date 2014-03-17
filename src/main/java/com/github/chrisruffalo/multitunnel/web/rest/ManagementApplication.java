ec060000e218285e62cbd36aaa50a3893ce7d3e9
Chris Ruffalo <chris.ruffalo@gmail.com>
2014-03-17 11:54:19 -0400

    removed reflections scanning in favor of more reliable hard-coded service classes

--- src/main/java/com/github/chrisruffalo/multitunnel/web/rest/ManagementApplication.java
+++ src/main/java/com/github/chrisruffalo/multitunnel/web/rest/ManagementApplication.java
@@ -1,22 +1,12 @@
 package com.github.chrisruffalo.multitunnel.web.rest;
 
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Method;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
 import javax.ws.rs.core.Application;
 
-import org.reflections.Reflections;
-
-import com.github.chrisruffalo.multitunnel.web.ManagementServer;
+import com.github.chrisruffalo.multitunnel.web.rest.services.InfoService;
+import com.github.chrisruffalo.multitunnel.web.rest.services.TunnelManagementService;
 
 public class ManagementApplication extends Application {
 
@@ -25,28 +15,10 @@ public class ManagementApplication extends Application {
 	private final Set<Object> singletons;
 	
 	public ManagementApplication() {
-		// create markers
-		final Set<Class<? extends Annotation>> markers = new HashSet<>();
-		
-		markers.add(GET.class);
-		markers.add(PUT.class);
-		markers.add(POST.class);
-		markers.add(DELETE.class);
-		markers.add(Path.class);
-		markers.add(Produces.class);
-		markers.add(Consumes.class);
-		
-		// scan local package for REST services
-		String packageName = ManagementServer.class.getName();
-		packageName = packageName.substring(0, packageName.lastIndexOf(".") - 1);
-		
-		// create reflective scanner for given package
-		Reflections reflections = new Reflections(packageName);
-		
-		// scan and add types
+		// add types
 		this.types = new HashSet<>();
-		this.types.addAll(ManagementApplication.types(reflections, markers));
-		this.types.addAll(ManagementApplication.methods(reflections, markers));
+		this.types.add(InfoService.class);
+		this.types.add(TunnelManagementService.class);
 
 		// create singletons
 		this.singletons = new HashSet<>();
@@ -60,31 +32,6 @@ public class ManagementApplication extends Application {
 	@Override
 	public Set<Object> getSingletons() {
 		return this.singletons;
-	}
-
-	private static Set<Class<?>> types(Reflections reflections, Set<Class<? extends Annotation>> markers) {
-		
-		Set<Class<?>> found = new HashSet<>();
-		
-		for(Class<? extends Annotation> type : markers) {
-			found.addAll(reflections.getTypesAnnotatedWith(type, true));
-		}
-		
-		return found;
-	}
-	
-	private static Set<Class<?>> methods(Reflections reflections, Set<Class<? extends Annotation>> markers) {
-
-		Set<Class<?>> found = new HashSet<>();
-		
-		for(Class<? extends Annotation> type : markers) {
-			Set<Method> methods = reflections.getMethodsAnnotatedWith(type);
-			for(Method method : methods) {
-				found.add(method.getDeclaringClass());
-			}
-		}
-		
-		return found;
 	}
 
 }
